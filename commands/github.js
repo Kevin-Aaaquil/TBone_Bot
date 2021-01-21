592fd1cc6372e3978ef7dad84c5514d5a6c2f4f2
Kevin Aaaquil <eshansingh2409@gmail.com>
2021-01-21 23:16:25 +0530

    del

--- commands/github.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "help",
-    description: "this is a help command: Shows all the commands available",
-    execute(message, args) {
-        message.channel.send("hhtp://www.github.com/Kevin-Aaaquil");
-    }
-}
