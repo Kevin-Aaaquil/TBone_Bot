7aebf63804ff73e40a0ce9ef5cacac3d57fe90c7
Kevin Aaaquil <eshansingh2409@gmail.com>
2021-01-24 02:46:00 +0530

    hey

--- commands/aryan.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "aryan",
-    description: "this is a help command: Shows all the commands available",
-    execute(message, args) {
-        message.channel.send("Jaa Sreya ke pass Jaaa");
-    }
-}
--- commands/surya.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "surya",
-    description: "this is a help command: Shows all the commands available",
-    execute(message, args) {
-        message.channel.send("Chal anime Dekhte Hai");
-    }
-}
