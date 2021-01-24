d7d42eb1f935265fcb06cbad6b5777d801cc6a36
Kevin Aaaquil <eshansingh2409@gmail.com>
2021-01-24 11:40:17 +0530

    remove

--- commands/help.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    name: "help",
-    description: "this is a help command: Shows all the commands available",
-    execute(message, args) {
-        message.channel.send("These are the following commands I know \n +help \n +anu \n +esha \n +github \n +kriti \n +riti \n +ding \n +shashi \n +surya \n +aryan \n +batra \n +office \n +dance");
-    }
-}
-
--- commands/kriti.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "kriti",
-    description: "this is a help command: Shows all the commands available",
-    execute(message, args) {
-        message.channel.send("Hello BKJ");
-    }
-}
