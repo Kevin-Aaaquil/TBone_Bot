daba46d78f0cea4fe7332244eb36751365111f6a
Kevin Aaaquil <eshansingh2409@gmail.com>
2021-02-07 07:18:02 +0530

    -gm

--- commands/shashi.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "shashi",
-    description: "this is a help command: Shows all the commands available",
-    execute(message, args) {
-        message.channel.send("Kaisa hai BubbleButt");
-    }
-}
