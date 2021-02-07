4db72fc1bc9b1cc920d265049c2070cfdade3a99
Kevin Aaaquil <eshansingh2409@gmail.com>
2021-02-07 07:19:54 +0530

    -Primary commit

---
+++ commands/riti.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: "riti",
+    description: "this is a help command: Shows all the commands available",
+    execute(message, args) {
+        message.channel.send("Hey Smartass!!!");
+        //message.channel.send("https://cdn.discordapp.com/attachments/780875930104365076/803980670257856522/WhatsApp_Image_2020-12-15_at_10.57.44_AM.jpeg");
+        //message.channel.send("https://tenor.com/view/slap-angry-kopam-serious-gif-gif-19001058");
+
+    }
+
+}
---
+++ commands/shashi.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "shashi",
+    description: "Shashank Saved us",
+    execute(message, args) {
+        message.channel.send("https://tenor.com/view/warrior-nun-future-warrior-nuns-mother-superion-sylvia-de-fanti-gods-champion-gif-18070043");
+    }
+}
