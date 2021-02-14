6ed1ffca1c437839c5b1032715c396a8ebe3dc5d
Kevin Aaaquil <eshansingh2409@gmail.com>
2021-02-15 00:12:46 +0530

    Delete shashi.js

--- commands/shashi.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "shashi",
-    description: "Shashank Saved us",
-    execute(message, args) {
-        message.channel.send("https://tenor.com/view/warrior-nun-future-warrior-nuns-mother-superion-sylvia-de-fanti-gods-champion-gif-18070043");
-    }
-}
