4d3fafe68d426f4106919ce194f5a80eccef455d
Kevin Aaaquil <eshansingh2409@gmail.com>
2021-02-15 00:13:45 +0530

    Delete baruni.js

--- commands/baruni.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "baruni",
-    description: "Brownie: urf Eid Ka Chaand",
-    execute(message, args) {
-        message.channel.send("https://tenor.com/view/poop-shit-diarrhea-ice-cream-soft-serve-gif-4971272");
-
-    }
-}
