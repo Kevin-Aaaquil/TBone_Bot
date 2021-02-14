50cdd0a471d1c6bde3054fc91d35e8ede765b528
Kevin Aaaquil <eshansingh2409@gmail.com>
2021-02-15 00:13:23 +0530

    Delete anu.js

--- commands/anu.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "anu",
-    description: "this is a help command: Shows all the commands available",
-    execute(message, args) {
-        message.channel.send("Ro mat Anuuuu");
-    }
-}
