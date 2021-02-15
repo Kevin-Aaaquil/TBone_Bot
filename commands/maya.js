a7d7fb6d67f3b537ed425ddcf9ea77eb1f14658b
Kevin Aaaquil <eshansingh2409@gmail.com>
2021-02-15 21:32:59 +0530

    ⚡️

--- commands/maya.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "maya",
-    description: "maya lund hai",
-    execute(message, args) {
-        message.channel.send("https://cdn.discordapp.com/attachments/768427821755793428/802974746396327936/PicsArt_01-25-12.15.31.jpg");
-        message.channel.send("https://cdn.discordapp.com/attachments/768427821755793428/802974746752319488/PicsArt_01-25-12.11.44.jpg");
-
-    }
-}
