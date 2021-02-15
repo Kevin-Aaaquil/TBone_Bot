e68041331662ee935de6b16dd573eefae1f169e5
Kevin Aaaquil <eshansingh2409@gmail.com>
2021-02-15 19:48:42 +0530

    fixed bug

---
+++ commands/baruni.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "baruni",
+    description: "Brownie: urf Eid Ka Chaand",
+    execute(message, args) {
+        message.channel.send("https://tenor.com/view/poop-shit-diarrhea-ice-cream-soft-serve-gif-4971272");
+
+    }
+}
---
+++ commands/shashi.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "shashi",
+    description: "Shashank Saved us",
+    execute(message, args) {
+        message.channel.send("https://cdn.discordapp.com/attachments/781903652448501820/808408895231361074/IMG-20210205-WA0041.jpg");
+    }
+}
