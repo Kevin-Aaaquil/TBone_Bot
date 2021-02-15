c9819c9c36149c5619171e115a768dc917742548
Kevin Aaaquil <eshansingh2409@gmail.com>
2021-02-15 19:40:06 +0530

    hidden token

---
+++ commands/hbd.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: "hbd",
+    description: "Birthday Wishes",
+    execute(message, args) {
+        message.channel.send("https://tenor.com/view/happy-birthday-to-you-minions-singing-gif-15506821");
+        message.channel.send("https://tenor.com/view/happy-birthday-alice-birthday-cake-blow-the-candle-gif-5219897");
+        message.channel.send("https://tenor.com/view/party-the-office-steve-carell-happy-birthday-gif-15262800");
+        message.channel.send("https://tenor.com/view/badbunny-yay-gif-18626862");
+        message.channel.send("https://tenor.com/view/chris-evans-captain-america-naked-evans-naked-happy-birthday-gif-17723693");
+        message.channel.send("https://tenor.com/view/happy-boob-day-bouncy-boobs-happy-breat-day-breast-boobs-gif-15039264");
+    }
+}
