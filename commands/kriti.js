425ed2c1f5e0748965364fa6c5ca6e44688f1af0
Kevin Aaaquil <eshansingh2409@gmail.com>
2021-02-15 19:37:30 +0530

    fuck

--- commands/kriti.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "kriti",
-    description: "this is a help command: Shows all the commands available",
-    execute(message, args) {
-        message.channel.send("https://cdn.discordapp.com/attachments/768427821755793428/802974746965704794/PicsArt_01-25-12.05.05.jpg");
-    }
-}
--- commands/shashi.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "shashi",
-    description: "Shashank Saved us",
-    execute(message, args) {
-        message.channel.send("https://cdn.discordapp.com/attachments/781903652448501820/808408895231361074/IMG-20210205-WA0041.jpg");
-    }
-}
