7bf88e8d3d73333655c6fc7f26166921bba075d0
Kevin Aaaquil <eshansingh2409@gmail.com>
2021-02-15 19:47:45 +0530

    fuck

--- commands/batra.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "batra",
-    description: "Harshit Batra",
-    execute(message, args) {
-        message.channel.send("Hume Parivaar me shamil karne ke liye shukriya");
-
-    }
-}
--- commands/ding.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: "ding",
-    description: "this is a help command: Shows all the commands available",
-    execute(message, args) {
-        message.channel.send("dong!");
-    }
-}
